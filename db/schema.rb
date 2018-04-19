6bedb2064ba501922cb9f2b8343c210436cd500a
itzbernoulli <oyinloyeayodeji@gmail.com>
2018-04-19 22:18:18 +0100

    User Roles setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419165919) do
+ActiveRecord::Schema.define(version: 20180419202723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_user_roles_on_role_id", using: :btree
+    t.index ["user_id"], name: "index_user_roles_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,4 +50,6 @@ ActiveRecord::Schema.define(version: 20180419165919) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "user_roles", "roles"
+  add_foreign_key "user_roles", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = ["Ayo", "Deji", "John", "Tobi", "Sam", "Monica", "Stella"]
+
+name_len = names.length
+
+roles = Role.all
+
+role_len = roles.length
+
+
+name_len.times do |n|
+	user = User.create(
+		first_name: names[n],
+		last_name: names[n],
+		email: names[n]+"@gmail.com",
+		password: names[n]+"@gmail.com")
+
+	r = Random.new
+	r = r.rand(0..role_len)
+ 
+	UserRole.create(
+		user: user,
+		role: roles[r])
+end
