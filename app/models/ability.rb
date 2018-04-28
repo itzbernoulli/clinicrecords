6d1571f1618404840406f938247a4c3c42790df5
itzbernoulli <oyinloyeayodeji@gmail.com>
2018-04-28 15:14:12 +0100

    devise authentication added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
 
     user ||= User.new # guest user (not logged in)
-    if user.admin?
+    if user.is_admin?
       can :manage, :all
     else
       can :read, :all
