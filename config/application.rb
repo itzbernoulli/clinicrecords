d8a73e42c1d77d635056fdd0a1d485106c826cd4
Ayodeji <oyinloyeayodeji@gmail.com>
2018-10-14 17:13:03 +0100

    added another config line

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module First
   class Application < Rails::Application
   	config.assets.paths << Rails.root.join("app", "assets", "fonts")
   	config.assets.initialize_on_precompile = false
+
+  	config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
