ff4912e80073f47c1464f36cfdfb23f584055aab
Ayodeji <oyinloyeayodeji@gmail.com>
2019-03-22 13:20:51 +0100

    included new layout file and modified the index files and forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  	layout :choose_layout
+
     protected  
     def after_sign_in_path_for(resource) 
     	if resource.is_admin?
@@ -8,5 +10,15 @@ class ApplicationController < ActionController::Base
     	else
     		records_path
     	end        
+    end
+
+    protected
+
+    def choose_layout
+    	if current_user.nil?
+    		"application"
+    	else
+    		"admin"
+    	end
     end
 end
