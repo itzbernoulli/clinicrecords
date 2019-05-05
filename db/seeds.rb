f018015f5645d7bacc3c6bc075b97bf838582502
Ayodeji <oyinloyeayodeji@gmail.com>
2019-05-06 00:49:20 +0100

    Modified files for ordering and deleting RecordIcd in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 # #
 # #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # #   Character.create(name: 'Luke', movie: movies.first)
+RecordIcd.delete_all
 Record.delete_all
 Department.delete_all
 UserRole.delete_all
